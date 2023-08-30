0bb8be975c4abcfe9832f9a8492dc076a8e669ba
Ton <ton@jarrbo.nl>
2023-08-30 19:04:55 +0200

    replace ugettxt with gettxt

--- cms_menus.py
+++ cms_menus.py
@@ -1,6 +1,6 @@
 from menus.base import NavigationNode, Modifier
 from menus.menu_pool import menu_pool
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 
 # Add a navigation node to the end of the existing menu
